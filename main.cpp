b9a60d80d643addc3dbfd4d57a34e512834021a6
mauricioudx <127424616+mauricioudx@users.noreply.github.com>
2024-03-05 19:35:03 +0100

    Add files via upload
    Practica 2 de interrupciones

--- main.cpp
+++ main.cpp
@@ -1,36 +1,31 @@
 #include <Arduino.h>
-#define LED_BUILTIN 23
-#define DELAY 500000
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-
-}
-
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("ON");
-  delay(DELAY);
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("OFF");
-  delay(DELAY);
-}
 struct Button {
-  const uint8_t PIN;
-  uint32_t numberKeyPresses;
-  bool pressed;
-  };
-
+const uint8_t PIN;
+uint32_t numberKeyPresses;
+bool pressed;
+};
 Button button1 = {18, 0, false};
-
-void pepe (){
-  button1.numberKeyPresses += 1;
-  button1.pressed = true;
+void IRAM_ATTR isr() {
+button1.numberKeyPresses += 1;
+button1.pressed = true;
 }
-
-void setup (){
-  Serial.begin (115200);
-  pinMode(button1.PIN, INPUT_PULLUP);
-  attachInterrupt(button1.PIN, pepe, FALLING);
+void setup() {
+Serial.begin(115200);
+pinMode(button1.PIN, INPUT_PULLUP);
+attachInterrupt(button1.PIN, isr, FALLING);
+}
+void loop() {
+if (button1.pressed) {
+Serial.printf("Button 1 has been pressed %u times\n",
+button1.numberKeyPresses);
+button1.pressed = false;
+}
+//Detach Interrupt after 1 Minute
+static uint32_t lastMillis = 0;
+if (millis() - lastMillis > 60000) {
+lastMillis = millis();
+detachInterrupt(button1.PIN);
+Serial.println("Interrupt Detached!");
+}
 }
